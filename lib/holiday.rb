7bb956f7488a063e705f8cb21c8adb0c57777e6e
Lena Hou <lenahou2013@gmail.com>
2021-02-04 01:51:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,5 +82,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash[:summer][:Fourth Of July][0]
+holiday_hash[:summer][:fourth_of_july]
 end
